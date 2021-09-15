27a4e3bada685f81b8fbc6e0cde9887ba14a3781
Kelvin Morrison <kelvinninja1@gmail.com>
2021-09-15 18:47:49 +0000

    Added: Student class
    *Output new Student as JSON

--- src/main/java/io/turntabl/springdemo/SpringDemoApplication.java
+++ src/main/java/io/turntabl/springdemo/SpringDemoApplication.java
@@ -1,10 +1,13 @@
 package io.turntabl.springdemo;
 
+import io.turntabl.springdemo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,13 @@ public class SpringDemoApplication {
     }
 
     @GetMapping
-    public List<String> hello(){
-        return List.of("Hello", "World");
+    public List<Student> hello(){
+        return List.of(new Student(
+                1L,
+                "Kelvin",
+                "man@ikelvin.co",
+                LocalDate.of(1998, Month.JULY, 2),
+                23));
     }
 
 }
